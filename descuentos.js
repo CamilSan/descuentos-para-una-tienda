e54e9ba490aa0312cd7396470bcceed7914ba25b
CamilSan <2241jose@gmail.com>
2022-04-15 17:48:24 -0500

    Convertir calculos en una funcion de JavaScript

--- descuentos.js
+++ descuentos.js
@@ -1,12 +1,16 @@
-const precioOriginal = 120;
-const descuento = 18;
+// const precioOriginal = 120;
+// const descuento = 18;
 
-const porcentajePrecioConDescuento = 100 - descuento;
-const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  
+    return precioConDescuento;
+}
 
-console.log({
-    precioOriginal,
-    descuento,
-    porcentajePrecioConDescuento,
-    precioConDescuento
-});
+// console.log({
+//   precioOriginal,
+//   descuento,
+//   porcentajePrecioConDescuento,
+//   precioConDescuento,
+// });
